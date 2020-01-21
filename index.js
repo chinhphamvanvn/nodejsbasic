99b885f4682430fdc4cdf8dd4d705deb69e78353
chinhpv <chinhpv@icheck.vn>
2020-01-21 11:17:38 +0700

    phan trang products

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+console.log(process.env.SESSION_SECRET);
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
 var userRoute = require('./routes/user.route');
+var productRoute = require('./routes/product.route');
 var authRoute = require('./routes/auth.route');
 
 var authMiddleware = require('./middlewares/auth.middleware');
@@ -15,7 +19,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser('sssssdfsafs'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.get('/', function(req, res){
     res.render('index', {
@@ -24,6 +28,7 @@ app.get('/', function(req, res){
 })
 
 app.use('/users', authMiddleware.requireAuth, userRoute);
+app.use('/products', authMiddleware.requireAuth, productRoute);
 app.use('/auth', authRoute);
 
 app.listen(port, function() {
